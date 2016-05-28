0a509cd281931c99575c38aa784fdbc89a87d3ca
Administrador <Administrador@JHONI-NOTE>
2016-05-28 13:36:40 -0300

    Melhorado layout do filtro para consulta de dados

--- src/main/java/com/geoinfo/bean/PeriodoBean.java
+++ src/main/java/com/geoinfo/bean/PeriodoBean.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -22,11 +23,12 @@ import javax.servlet.http.HttpSession;
 @SessionScoped
 public class PeriodoBean implements Serializable{
     private List<EPeriodoType> listaIdPeriodo;
-    private List<Integer> listaIdDia;
+    //private List<Integer> listaIdDia;
     private List<EPeriodoMesType> listaIdMes;
     private List<Integer> listaIdAno;
-    private int idAnoA, idAnoB, idAnoC, idAnoD, idDiaA, idDiaB, idDiaC, idDiaD;
-    private EPeriodoMesType idMesA, idMesB, idMesC, idMesD;
+    //private int idAnoA, idAnoB, idAnoC, idAnoD, idDiaA, idDiaB, idDiaC, idDiaD;
+    //private EPeriodoMesType idMesA, idMesB, idMesC, idMesD;
+    private Date dtPeriodoA, dtPeriodoB, dtPeriodoC, dtPeriodoD;
     private boolean inCompararCom, inAteAB, inAteCD;
     private EPeriodoType idPeriodoAB, idPeriodoCD;
     
@@ -34,9 +36,9 @@ public class PeriodoBean implements Serializable{
         this.listaIdPeriodo = new ArrayList<EPeriodoType>();
         this.listaIdPeriodo.addAll(Arrays.asList(EPeriodoType.values()));
         
-        this.listaIdDia = new ArrayList<Integer>();
+        /*this.listaIdDia = new ArrayList<Integer>();
         for(int i = 1; i < 32; i++)
-            this.listaIdDia.add(i);
+            this.listaIdDia.add(i);*/
         
         this.listaIdMes = new ArrayList<EPeriodoMesType>();
         this.listaIdMes.addAll(Arrays.asList(EPeriodoMesType.values()));
@@ -46,27 +48,31 @@ public class PeriodoBean implements Serializable{
             this.listaIdAno.add(i);
         
         idPeriodoAB = EPeriodoType.ANO;
-        idAnoA = Calendar.getInstance().get(Calendar.YEAR);
-        idMesA = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
-        idDiaA = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        dtPeriodoA = Calendar.getInstance().getTime();
+        //idAnoA = Calendar.getInstance().get(Calendar.YEAR);;
+        //idMesA = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
+        //idDiaA = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         
         inAteAB = false;
         
-        idAnoB = Calendar.getInstance().get(Calendar.YEAR);
-        idMesB = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
-        idDiaB = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        dtPeriodoB = Calendar.getInstance().getTime();
+        //idAnoB = Calendar.getInstance().get(Calendar.YEAR);
+        //idMesB = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
+        //idDiaB = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         
         
         idPeriodoCD = EPeriodoType.ANO;
-        idAnoC = Calendar.getInstance().get(Calendar.YEAR);
-        idMesC = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
-        idDiaC = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        dtPeriodoC = Calendar.getInstance().getTime();
+        //idAnoC = Calendar.getInstance().get(Calendar.YEAR);
+        //idMesC = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
+        //idDiaC = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         
         inAteCD = false;
         
-        idAnoD = Calendar.getInstance().get(Calendar.YEAR);
-        idMesD = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
-        idDiaD = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        dtPeriodoD = Calendar.getInstance().getTime();
+        //idAnoD = Calendar.getInstance().get(Calendar.YEAR);
+        //idMesD = EPeriodoMesType.values()[Calendar.getInstance().get(Calendar.MONTH)];
+        //idDiaD = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
     }
 
     public List<EPeriodoType> getListaIdPeriodo() {
@@ -77,13 +83,13 @@ public class PeriodoBean implements Serializable{
         this.listaIdPeriodo = listaIdPeriodo;
     }
 
-    public List<Integer> getListaIdDia() {
+    /*public List<Integer> getListaIdDia() {
         return this.listaIdDia;
     }
 
     public void setListaIdDia(List<Integer> listaIdDia) {
         this.listaIdDia = listaIdDia;
-    }
+    }*/
 
     public List<EPeriodoMesType> getListaIdMes() {
         return this.listaIdMes;
@@ -102,99 +108,191 @@ public class PeriodoBean implements Serializable{
     }
 
     public int getIdAnoA() {
-        return idAnoA;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoA);
+        return calendar.get(Calendar.YEAR);
     }
 
     public void setIdAnoA(int idAnoA) {
-        this.idAnoA = idAnoA;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoA);
+        calendar.set(Calendar.YEAR, idAnoA);
+        this.dtPeriodoA = calendar.getTime();
     }
 
     public int getIdAnoB() {
-        return idAnoB;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoB);
+        return calendar.get(Calendar.YEAR);
     }
 
     public void setIdAnoB(int idAnoB) {
-        this.idAnoB = idAnoB;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoB);
+        calendar.set(Calendar.YEAR, idAnoB);
+        this.dtPeriodoB = calendar.getTime();
     }
 
     public int getIdAnoC() {
-        return idAnoC;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoC);
+        return calendar.get(Calendar.YEAR);
     }
 
     public void setIdAnoC(int idAnoC) {
-        this.idAnoC = idAnoC;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoC);
+        calendar.set(Calendar.YEAR, idAnoC);
+        this.dtPeriodoC = calendar.getTime();
     }
 
     public int getIdAnoD() {
-        return idAnoD;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoD);
+        return calendar.get(Calendar.YEAR);
     }
 
     public void setIdAnoD(int idAnoD) {
-        this.idAnoD = idAnoD;
-    }
-
-    public int getIdDiaA() {
-        return idDiaA;
-    }
-
-    public void setIdDiaA(int idDiaA) {
-        this.idDiaA = idDiaA;
-    }
-
-    public int getIdDiaB() {
-        return idDiaB;
-    }
-
-    public void setIdDiaB(int idDiaB) {
-        this.idDiaB = idDiaB;
-    }
-
-    public int getIdDiaC() {
-        return idDiaC;
-    }
-
-    public void setIdDiaC(int idDiaC) {
-        this.idDiaC = idDiaC;
-    }
-
-    public int getIdDiaD() {
-        return idDiaD;
-    }
-
-    public void setIdDiaD(int idDiaD) {
-        this.idDiaD = idDiaD;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoD);
+        calendar.set(Calendar.YEAR, idAnoD);
+        this.dtPeriodoD = calendar.getTime();
     }
 
     public EPeriodoMesType getIdMesA() {
-        return idMesA;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoA);
+        return EPeriodoMesType.values()[calendar.get(Calendar.MONTH)];
     }
 
     public void setIdMesA(EPeriodoMesType idMesA) {
-        this.idMesA = idMesA;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoA);
+        calendar.set(Calendar.MONTH, idMesA.ordinal());
+        this.dtPeriodoA = calendar.getTime();
     }
 
     public EPeriodoMesType getIdMesB() {
-        return idMesB;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoB);
+        return EPeriodoMesType.values()[calendar.get(Calendar.MONTH)];
     }
 
     public void setIdMesB(EPeriodoMesType idMesB) {
-        this.idMesB = idMesB;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoB);
+        calendar.set(Calendar.MONTH, idMesB.ordinal());
+        this.dtPeriodoB = calendar.getTime();
     }
 
     public EPeriodoMesType getIdMesC() {
-        return idMesC;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoC);
+        return EPeriodoMesType.values()[calendar.get(Calendar.MONTH)];
     }
 
     public void setIdMesC(EPeriodoMesType idMesC) {
-        this.idMesC = idMesC;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoC);
+        calendar.set(Calendar.MONTH, idMesC.ordinal());
+        this.dtPeriodoC = calendar.getTime();
     }
 
     public EPeriodoMesType getIdMesD() {
-        return idMesD;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoD);
+        return EPeriodoMesType.values()[calendar.get(Calendar.MONTH)];
     }
 
     public void setIdMesD(EPeriodoMesType idMesD) {
-        this.idMesD = idMesD;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoD);
+        calendar.set(Calendar.MONTH, idMesD.ordinal());
+        this.dtPeriodoD = calendar.getTime();
+    }
+
+    public int getIdDiaA() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoA);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public void setIdDiaA(int idDiaA) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoA);
+        calendar.set(Calendar.DAY_OF_MONTH, idDiaA);
+        this.dtPeriodoA = calendar.getTime();
+    }
+
+    public int getIdDiaB() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoB);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public void setIdDiaB(int idDiaB) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoB);
+        calendar.set(Calendar.DAY_OF_MONTH, idDiaB);
+        this.dtPeriodoB = calendar.getTime();
+    }
+
+    public int getIdDiaC() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoC);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public void setIdDiaC(int idDiaC) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoC);
+        calendar.set(Calendar.DAY_OF_MONTH, idDiaC);
+        this.dtPeriodoC = calendar.getTime();
+    }
+
+    public int getIdDiaD() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoD);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public void setIdDiaD(int idDiaD) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dtPeriodoD);
+        calendar.set(Calendar.DAY_OF_MONTH, idDiaD);
+        this.dtPeriodoD = calendar.getTime();
+    }
+
+    public Date getDtPeriodoA() {
+        return dtPeriodoA;
+    }
+
+    public void setDtPeriodoA(Date dtPeriodoA) {
+        this.dtPeriodoA = dtPeriodoA;
+    }
+
+    public Date getDtPeriodoB() {
+        return dtPeriodoB;
+    }
+
+    public void setDtPeriodoB(Date dtPeriodoB) {
+        this.dtPeriodoB = dtPeriodoB;
+    }
+
+    public Date getDtPeriodoC() {
+        return dtPeriodoC;
+    }
+
+    public void setDtPeriodoC(Date dtPeriodoC) {
+        this.dtPeriodoC = dtPeriodoC;
+    }
+
+    public Date getDtPeriodoD() {
+        return dtPeriodoD;
+    }
+
+    public void setDtPeriodoD(Date dtPeriodoD) {
+        this.dtPeriodoD = dtPeriodoD;
     }
 
     public boolean isInCompararCom() {
@@ -248,7 +346,7 @@ public class PeriodoBean implements Serializable{
 
         if(!inErro){
             try{
-                periodoIntervaloA = PeriodoIntervalo.create(idPeriodoAB, idAnoA, idMesA, idDiaA, idAnoB, idMesB, idDiaB, inAteAB);
+                periodoIntervaloA = PeriodoIntervalo.create(idPeriodoAB, getIdAnoA(), getIdMesA(), getIdDiaA(), getIdAnoB(), getIdMesB(), getIdDiaB(), inAteAB);
             }catch (PeriodoControlException pce){
                 FacesMessage fm = new FacesMessage(pce.getMessage());
                 fm.setSeverity(FacesMessage.SEVERITY_ERROR);
@@ -259,7 +357,7 @@ public class PeriodoBean implements Serializable{
         
         if(!inErro){
             try{
-                periodoIntervaloB = PeriodoIntervalo.create(idPeriodoCD, idAnoC, idMesC, idDiaC, idAnoD, idMesD, idDiaD, inAteCD);
+                periodoIntervaloB = PeriodoIntervalo.create(idPeriodoCD, getIdAnoC(), getIdMesC(), getIdDiaC(), getIdAnoD(), getIdMesD(), getIdDiaD(), inAteCD);
             }catch (PeriodoControlException pce){
                 FacesMessage fm = new FacesMessage(pce.getMessage());
                 fm.setSeverity(FacesMessage.SEVERITY_ERROR);
